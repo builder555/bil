affa501e2e3fb9de5e34f4c616340c22326b5f8b
builder555 <85308587+builder555@users.noreply.github.com>
2024-06-27 07:28:29 -0400

    fix: remove commented code

--- api/main.py
+++ api/main.py
@@ -5,7 +5,7 @@ from fastapi.responses import HTMLResponse
 from dbfile import DBAdaptor
 import json
 
-app = FastAPI()
+app = FastAPI(title="bil-api")
 
 origins = [
     # "http://localhost",
@@ -33,46 +33,6 @@ async def list_projects():
 async def add_a_new_project(name: str):
     return db.add_project(name)
 
-
-# @app.put('/projects/{project_id}')
-# async def update_project(project_id: int, details: Project):
-#     return db.update_project(project_id, details.dict())
-
-# @app.delete('/projects/{project_id}')
-# async def delete_project(project_id: int):
-#     return db.delete_project(project_id)
-
-# @app.get('/projects/{project_id}/groups')
-# async def payment_groups_in_a_project(project_id: int):
-#     return db.get_paygroups(project_id)
-
-# @app.post('/groups')
-# async def add_a_new_payment_group(details: PaygroupBase):
-#     return db.add_payment_group(details.dict())
-
-# @app.put('/groups/{group_id}')
-# async def update_payment_group(group_id: int, details: PaygroupBase):
-#     return db.update_group(group_id, details.dict())
-
-# @app.delete('/groups/{group_id}')
-# async def delete_payment_group(group_id: int):
-#     return db.delete_group(group_id)
-
-# @app.get('/groups/{group_id}/payments')
-# async def individual_group_payments(group_id: int):
-#     return db.get_payments(group_id)
-
-# @app.post('/payments')
-# async def add_new_payment(details: Payment):
-#     return db.add_payment(details.dict())
-
-# @app.put('/payments/{pay_id}')
-# async def update_payment(pay_id: int, details: Payment):
-#     return db.update_payment(pay_id, details.dict())
-
-# @app.delete('/payments/{pay_id}')
-# async def delete_payment(pay_id: int):
-#     return db.delete_payment(pay_id)
 
 app.mount("/css", StaticFiles(directory="dist/css"), name="static")
 app.mount("/js", StaticFiles(directory="dist/js"), name="static")
