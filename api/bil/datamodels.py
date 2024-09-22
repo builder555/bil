f43705b99feb94b809c42ea414f464509543cbb5
builder555 <85308587+builder555@users.noreply.github.com>
2024-09-22 09:53:44 -0400

    feat: use more generic asset/liability for Payment. only one is required

--- api/bil/datamodels.py
+++ api/bil/datamodels.py
@@ -1,7 +1,23 @@
 from sqlite3.dbapi2 import Date
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator, ValidationInfo
+from typing_extensions import Self
 import json
+
+
+class PaymentInput(BaseModel):
+    name: str
+    date: Date
+    asset: Optional[int] = 0
+    liability: Optional[int] = 0
+
+    @model_validator(mode="after")
+    def must_have_either_asset_or_liability(
+        self,
+    ) -> Self:
+        if self.asset == 0 and self.liability == 0:
+            raise ValueError("Must specify either asset or liability")
+        return self
 
 
 class PaygroupBase(BaseModel):
@@ -10,7 +26,6 @@ class PaygroupBase(BaseModel):
 
 class Paygroup(PaygroupBase):
     id: int
-
 
 class ProjectResponse(BaseModel):
     id: int
@@ -22,7 +37,7 @@ class Project(ProjectResponse):
 
 
 class ProjectWithPayments(ProjectResponse):
-    paygroups: dict[int, Paygroup]
+    paygroups: list[Paygroup]
 
 
 class ProjectEncoder(json.JSONEncoder):
@@ -30,11 +45,3 @@ class ProjectEncoder(json.JSONEncoder):
         if issubclass(type(obj), BaseModel):
             return obj.model_dump()
         return super().default(obj)
-
-
-class Payment(BaseModel):
-    name: str
-    when: Date
-    paygroup: int
-    amount: float
-    owed: float
