01f2b6fe1ca907fcafb893374b3c6684a2bc9d00
builder555 <85308587+builder555@users.noreply.github.com>
2024-09-22 09:55:03 -0400

    feat: adding payments, use consistent data structures

--- api/bil/dbfile.py
+++ api/bil/dbfile.py
@@ -1,8 +1,7 @@
-from datamodels import Paygroup, Project, ProjectEncoder, ProjectWithPayments
+from bil.datamodels import PaymentInput, Paygroup, Project, ProjectEncoder, ProjectWithPayments
 import os
 import json
-
-from functools import lru_cache
+from dulwich.repo import Repo
 from typing import Mapping
 
 
@@ -14,7 +13,6 @@ class DBAdaptor:
     def _get_project_path(self, project_id: int) -> str:
         return os.path.join(self._base, f"projects/{project_id}")
 
-    @lru_cache
     def _get_projects_dict(self, include_deleted: bool = False) -> dict[int, Project]:
         projects = {}
         if os.path.exists(self._db_path):
@@ -37,7 +35,6 @@ class DBAdaptor:
     def _save_projects(self, projects: dict[int, Project]):
         with open(self._db_path, "w") as f:
             json.dump(projects, f, cls=ProjectEncoder, indent=4)
-        self._get_projects_dict.cache_clear()
 
     def _get_next_id(self, items: Mapping[int, Project | Paygroup]) -> int:
         if not items:
@@ -47,20 +44,21 @@ class DBAdaptor:
     def _mk_project_dir(self, project_id: int):
         project_path = self._get_project_path(project_id)
         os.makedirs(project_path)
-        os.system(f"git init {project_path}")
+        Repo.init(project_path)
 
     def get_projects(self, include_deleted: bool = False) -> list[Project]:
         if include_deleted:
             return list(self._all_projects_dict.values())
         return list(self._projects_dict.values())
 
-    def add_project(self, name: str):
+    def add_project(self, name: str) -> int:
         projects = self._all_projects_dict
         new_id = self._get_next_id(projects)
         new_project = Project(name=name, id=new_id)
         self._mk_project_dir(new_id)
         projects[new_id] = new_project
         self._save_projects(projects)
+        return new_id
 
     def delete_project(self, project_id: int):
         projects = self._projects_dict
@@ -87,30 +85,36 @@ class DBAdaptor:
         with open(payfile_path, "w") as f:
             json.dump(paygroups, f, cls=ProjectEncoder, indent=4)
 
-    def add_paygroup(self, project_id: int, name: str):
-        paygroups = self.get_paygroups(project_id)
-        new_group_id = self._get_next_id(paygroups)
-        new_paygroup = Paygroup(id=new_group_id, name=name)
-        paygroups[new_group_id] = new_paygroup
+    def add_paygroup(self, project_id: int, name: str) -> int:
+        paygroups = self._get_paygroups_dict(project_id)
+        new_id = self._get_next_id(paygroups)
+        new_paygroup = Paygroup(id=new_id, name=name)
+        paygroups[new_id] = new_paygroup
         self._save_paygroups(project_id, paygroups)
+        return new_id
 
-    def get_paygroups(self, project_id: int) -> dict[int, Paygroup]:
+    def _get_paygroups_dict(self, project_id: int) -> dict[int, Paygroup]:
         if project_id not in self._projects_dict:
             raise ItemNotFoundError
         groups = {}
         payfile_path = self._get_payfile_path(project_id)
         if os.path.exists(payfile_path):
-            with open(self._get_project_path(project_id) + "/payments.json", "r") as f:
+            with open(self._get_payfile_path(project_id), "r") as f:
                 groups = json.load(f)
         return {int(k): Paygroup(**v) for k, v in groups.items()}
 
+    def get_paygroups(self, project_id: int) -> list[Paygroup]:
+        return list(self._get_paygroups_dict(project_id).values())
+
     def delete_paygroup(self, project_id: int, paygroup_id: int):
-        groups = self.get_paygroups(project_id)
+        groups = self._get_paygroups_dict(project_id)
         if paygroup_id not in groups:
             raise ItemNotFoundError
         del groups[paygroup_id]
         self._save_paygroups(project_id, groups)
 
+    def add_payment(self, project_id: int, paygroup_id: int, payment: PaymentInput):
+        pass
 
 class ItemNotFoundError(Exception):
     pass
--- api/bil/test_dbfile.py
+++ api/bil/test_dbfile.py
@@ -1,14 +1,14 @@
 import os
 import shutil
-from datamodels import Paygroup, PaygroupBase, Project
-from dbfile import DBAdaptor, ItemNotFoundError
+from bil.datamodels import PaymentInput, Paygroup, Project
+from bil.dbfile import DBAdaptor, ItemNotFoundError
 import pytest
 
 DIR = "test_data"
 
 
 @pytest.fixture
-def db():
+def db(fs):
     yield DBAdaptor(DIR)
     if os.path.exists(DIR):
         shutil.rmtree(DIR)
@@ -19,18 +19,18 @@ def test_getting_projects_from_blank_db_returns_empty_list(db: DBAdaptor):
 
 
 def test_adding_new_project_creates_directory_with_git(db: DBAdaptor):
-    db.add_project(name="test")
-    assert os.path.exists(os.path.join(DIR, "projects", "1"))
-    assert os.path.exists(os.path.join(DIR, "projects", "1", ".git"))
+    new_id = db.add_project(name="test")
+    assert os.path.exists(os.path.join(DIR, "projects", f"{new_id}"))
+    assert os.path.exists(os.path.join(DIR, "projects", f"{new_id}", ".git"))
 
 
 def test_can_get_all_projects(db: DBAdaptor):
-    db.add_project(name="test1")
-    db.add_project(name="test2")
+    id1 = db.add_project(name="test1")
+    id2 = db.add_project(name="test2")
     projects = db.get_projects()
     assert len(projects) == 2
-    assert projects[0] == Project(name="test1", id=1)
-    assert projects[1] == Project(name="test2", id=2)
+    assert projects[0] == Project(name="test1", id=id1)
+    assert projects[1] == Project(name="test2", id=id2)
 
 
 def test_soft_deleting_project_removes_it_from_db(db: DBAdaptor):
@@ -60,13 +60,13 @@ def test_deleting_same_project_twice_raises_error(db: DBAdaptor):
 def test_can_add_new_paygroup(db: DBAdaptor):
     db.add_project(name="test")
     db.add_paygroup(project_id=1, name="test paygroup")
-    assert db.get_paygroups(project_id=1) == {1: Paygroup(id=1, name="test paygroup")}
+    assert db.get_paygroups(project_id=1) == [Paygroup(id=1, name="test paygroup")]
 
 
 def test_can_get_paygroups(db: DBAdaptor):
     db.add_project(name="test")
     db.add_paygroup(project_id=1, name="test paygroup")
-    assert db.get_paygroups(project_id=1) == {1: Paygroup(id=1, name="test paygroup")}
+    assert db.get_paygroups(project_id=1) == [Paygroup(id=1, name="test paygroup")]
 
 
 def test_getting_paygroups_for_nonexistent_project_raises_error(db: DBAdaptor):
@@ -87,5 +87,30 @@ def test_can_delete_paygroup(db: DBAdaptor):
     assert not db.get_paygroups(project_id=1)
 
 
+def test_cannot_add_payment_without_asset_or_liability(db: DBAdaptor):
+    db.add_project(name="test")
+    db.add_paygroup(project_id=1, name="test paygroup")
+    with pytest.raises(ValueError):
+        payment = PaymentInput(name="test payment", date="2022-01-01")
+        db.add_payment(project_id=1, paygroup_id=1, payment=payment)
+
+
+def test_can_add_payment_with_asset_only(db: DBAdaptor):
+    db.add_project(name="test")
+    db.add_paygroup(project_id=1, name="test paygroup")
+    payment = PaymentInput(name="test payment", date="2022-01-01", asset=100)
+    db.add_payment(project_id=1, paygroup_id=1, payment=payment)
+    # assert db.get_payments(project_id=1) == {1: [payment]}
+
+
+def test_can_add_payment_with_liability_only(db: DBAdaptor):
+    db.add_project(name="test")
+    db.add_paygroup(project_id=1, name="test paygroup")
+    payment = PaymentInput(name="test payment", date="2022-01-01", liability=100)
+    db.add_payment(project_id=1, paygroup_id=1, payment=payment)
+
+
+@pytest.mark.skip
 def test_can_view_previous_states_of_project(db: DBAdaptor):
+    # WHEN THIS IS IMPLEMENTED, REMOVE THE TEST CHECKING FOR .git IN THE PROJECTS DIRECTORY
     pass
