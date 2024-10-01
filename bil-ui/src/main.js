3714f8b672e5a8364c07beeee0f45c6918d85f90
builder555 <85308587+builder555@users.noreply.github.com>
2024-10-01 07:30:50 -0400

    fix: made some styles global

--- bil-ui/src/main.js
+++ bil-ui/src/main.js
@@ -4,6 +4,7 @@ import router from './router';
 import vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import './assets/main.css';
 
 Vue.config.productionTip = false;
 
