09cf227563f7dd0893a9548045c10e58e4aa00cd
builder555 <85308587+builder555@users.noreply.github.com>
2024-10-03 17:36:33 -0400

    fix: allow remote access in dev

--- bil-ui/vue.config.js
+++ bil-ui/vue.config.js
@@ -2,6 +2,12 @@ module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
+  devServer: {
+    disableHostCheck: true,
+    host: '0.0.0.0',
+    // public: '0.0.0.0:8081',
+    // https: true
+  },
   // publicPath: '',
   // publicPath: `${process.env.NODE_ENV === 'development' ? '' : '/bil/'}`,
 };
